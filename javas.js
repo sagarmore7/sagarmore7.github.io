9d2c70d18c349ffdb400c7010debabcaae6598ab
sagarmore7 <56201579+sagarmore7@users.noreply.github.com>
2019-10-07 21:47:26 +0530

    Uploading website

---
+++ javas.js
@@ -0,0 +1,34 @@
+function myf(){
+    document.getElementById("morecontent").innerHTML="Magic!!";
+    document.getElementById("morecontent").style.fontSize="30px";
+    document.getElementById("morecontent").style.color="Red";
+    document.getElementById("morecontent").style.textAlign="center";
+}
+console.log("Hello sagar")
+function abc(){
+var a,b,c;
+a=5;
+b=a;
+c=a+b;
+document.getElementById('a').innerHTML=c;}
+
+function myname()
+{
+    alert("           Hey sagar!!        ")
+}
+myname();
+/*
+var txt = " Hello".concat(" world asas asd world!");
+var str = "apple, banana, kiwi";
+var x = txt.length;
+var pos1 = txt.indexOf("w",15);
+var res = str.slice(7,13);
+document.write("string length is "+x);
+document.write("position of w is "+pos1);
+document.write(res);
+var title ="Hello world";
+title = title.toUpperCase("Hello","welcome");
+document.write(title.concat("<br>"));
+document.write(txt);document.write("<br>");
+document.write("<br>");
+*/
